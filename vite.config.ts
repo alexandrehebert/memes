a2811932e77f6de0b3b198c8f00a88dd3e3613c1
Alexandre Hebert <hebert.alex@gmail.com>
2025-04-19 18:41:29 -0400

    removed useless plugin

--- vite.config.ts
+++ vite.config.ts
@@ -9,29 +9,6 @@ export default defineConfig({
   plugins: [
     vue(),
     vuetify({ autoImport: true }), // Ensure Vuetify plugin is configured
-    {
-      name: 'vite-plugin-tournament-list-build',
-      apply: 'build',
-      async buildStart() {
-        const dataFolder = path.resolve(__dirname, '../../data/tournaments');
-        const outputFile = path.resolve(__dirname, '../../data/tournaments.json');
-
-        try {
-          const files = await fs.readdir(dataFolder);
-          const tournaments = files
-            .filter((file) => file.endsWith('.csv'))
-            .map((file) => ({
-              name: path.basename(file, '.csv'),
-              filePath: `/data/tournaments/${file}`,
-            }));
-
-          await fs.writeFile(outputFile, JSON.stringify(tournaments, null, 2));
-          console.log(`Generated tournaments.json with ${tournaments.length} tournaments.`);
-        } catch (error) {
-          console.error('Error generating tournaments.json:', error);
-        }
-      },
-    },
   ],
   css: {
     preprocessorOptions: {
