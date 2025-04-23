2596e4935dfc28a70b244dd3f834655febe13b62
Alexandre Hebert <hebert.alex@gmail.com>
2025-04-22 22:45:37 -0400

    smaller thumbnail

--- pages/[category]/[name].vue
+++ pages/[category]/[name].vue
@@ -77,7 +77,7 @@ const currentUrl = meme ? `${HOST}/${route.params.category}/${route.params.name}
 
 if (meme) {
   const img = useImage();
-  const src = img(meme.image, { width: 160, height: 160, quality: 70 });
+  const src = img(meme.image, { width: 100, height: 100, quality: 70 });
 
   useSeoMeta({
     title: meme.description,
